8315c4be8fef65cdd8706c41c8b7cfdf244a8a15
sooheon <tngjs0@gmail.com>
2016-05-26 11:29:33 +0900

    Assimilate more packages
    ace-link, elisp-slime-nav, evil-matchit, undo-tree, which-key,
    window-numbering.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eldoc-idle-delay 0)
  '(evil-leader/leader "SPC")
  '(evil-want-C-w-delete t)
  '(frame-resize-pixelwise t)
--- init.el
+++ init.el
@@ -26,6 +26,9 @@
   (require 'borg)
   (borg-initialize))
 
+(progn ;; themes
+  (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory)))
+
 (progn ;; `use-package'
   (eval-when-compile
     (require 'use-package))
@@ -60,6 +63,53 @@
 (use-package server
   :config (or (server-running-p) (server-mode)))
 
+(use-package evil
+  :init
+  (setq-default evil-want-C-u-scroll t
+                evil-want-fine-undo nil
+                evil-cross-lines t
+                evil-symbol-word-search t
+                evil-move-cursor-back nil
+                evil-want-C-i-jump t
+                evil-disable-insert-state-bindings t)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map "\C-w" 'evil-delete-backward-word))
+
+(use-package evil-leader
+  :init
+  (defun spacemacs/alternate-buffer ()
+    "Switch back and forth between current and last buffer in the
+current window."
+    (interactive)
+    (if (evil-alternate-buffer)
+	(switch-to-buffer (car (evil-alternate-buffer)))
+      (switch-to-buffer (other-buffer (current-buffer) t))))
+  (evil-leader/set-key
+    "TAB" 'spacemacs/alternate-buffer
+    "u" 'universal-argument
+    "wl" 'evil-window-right
+    "wh" 'evil-window-left
+    "wk" 'evil-window-top
+    "wj" 'evil-window-bottom
+    "wL" 'evil-window-move-far-right
+    "wH" 'evil-window-move-far-left
+    "wK" 'evil-window-move-very-top
+    "wJ" 'evil-window-move-very-bottom
+    "w=" 'balance-windows
+    "wm" 'delete-other-windows
+    "wo" 'other-window
+    "ws" 'evil-window-split
+    "wv" 'evil-window-vsplit)
+  :config
+  (global-evil-leader-mode))
+
+(use-package which-key
+  :diminish which-key-mode
+  :init
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5))
+
 (progn ; startup
   (message "Loading early birds...done (%.3fs)"
            (float-time (time-subtract (current-time)
@@ -75,8 +125,18 @@
   (setq avy-keys sooheon--avy-keys)
   (global-set-key [remap goto-line] 'evil-avy-goto-line))
 
+(use-package ace-link
+  :commands (ace-link-info ace-link-eww ace-link-help)
+  :init
+  (with-eval-after-load 'info (define-key Info-mode-map "o" 'ace-link-info))
+  (with-eval-after-load 'help-mode (define-key help-mode-map "o" 'ace-link-help)))
+
+(use-package autorevert
+  :diminish auto-revert-mode
+  :defer t)
+
 (use-package company
-  :diminish company-mode
+  :diminish (company-mode . "co")
   ;; :bind (:map company-active-map
   ;;             ("C-w" . nil)
   ;;             ("M-." . company-show-location)
@@ -91,9 +151,10 @@
   (setq company-idle-delay 0.2
         company-minimum-prefix-length 2)
   (global-company-mode)
-  (define-key company-active-map [escape] (lambda () (interactive)
-					    (company-abort)
-					    (evil-normal-state))))
+  (let ((m company-active-map))
+    (define-key m [escape] (lambda () (interactive)
+                             (company-abort)
+                             (evil-normal-state)))))
 
 (use-package dash
   :config (dash-enable-font-lock))
@@ -105,74 +166,98 @@
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh t))
 
 (use-package dired
-  :defer t
-  :config (setq dired-listing-switches "-alh"))
-
-(use-package eldoc
-  :config (global-eldoc-mode))
-
-(use-package evil
+  :commands (dired-jump)
   :init
-  (setq-default evil-want-C-u-scroll t
-                evil-want-fine-undo nil
-                evil-cross-lines t
-                evil-symbol-word-search t
-                evil-move-cursor-back nil
-                evil-want-C-i-jump t
-                evil-disable-insert-state-bindings t)
+  (evil-leader/set-key "d" 'dired-jump)
   :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map "\C-w" 'evil-delete-backward-word))
+  (setq dired-listing-switches "-alh"))
 
-(use-package evil-leader
+(use-package elisp-slime-nav
+  :diminish elisp-slime-nav-mode
   :config
-  (global-evil-leader-mode)
-  (defun spacemacs/alternate-buffer ()
-    "Switch back and forth between current and last buffer in the
-current window."
-    (interactive)
-    (if (evil-alternate-buffer)
-	(switch-to-buffer (car (evil-alternate-buffer)))
-      (switch-to-buffer (other-buffer (current-buffer) t))))
-  (evil-leader/set-key
-    "TAB" 'spacemacs/alternate-buffer))
+  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+    (add-hook hook 'turn-on-elisp-slime-nav-mode))
+  (evil-define-key 'normal elisp-slime-nav-mode-map
+    "K" 'elisp-slime-nav-describe-elisp-thing-at-point))
+
+(use-package evil-matchit
+  :defer t)
 
 (use-package help
-  :defer t
   :config (temp-buffer-resize-mode))
-
-(progn ;; `isearch'
-  (setq isearch-allow-scroll t))
 
 (use-package ivy
   :diminish ivy-mode
   :bind (("s-f" . swiper)
-	 ;;:map ivy-minibuffer-map
-	 ;;(([escape] . minibuffer-keyboard-quit)
-	 )
+         ("C-s" . swiper)
+         ("s-b" . ivy-switch-buffer)
+         ("C-c k" . counsel-ag)
+         ("M-x" . counsel-M-x)
+         ("C-h f" . counsel-describe-function)
+         ("C-h v" . counsel-describe-variable)
+         ("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-x l" . counsel-locate)
+         ("C-c C-r" . ivy-resume))
   :config
-  (setq ivy-use-virtual-buffers t)
+  (setq ivy-use-virtual-buffers t
+        ivy-extra-directories '("./")
+        ivy-count-format "%d "
+        ivy-height 12)
   (ivy-mode 1)
   (evil-leader/set-key
     "f" 'counsel-find-file
     "r" 'ivy-recentf
     "b" 'ivy-switch-buffer
-    "/" 'counsel-ag)
-  (define-key ivy-minibuffer-map [escape] 'minibuffer-keyboard-quit)
-  (global-set-key "\C-ck" 'counsel-ag)
-  (global-set-key "\M-x" 'counsel-M-x)
-  (global-set-key "\C-hf" 'counsel-describe-function)
-  (global-set-key "\C-hv" 'counsel-describe-variable)
-  ;; (global-set-key "\C-hl" 'counsel-load-library)
-  ;; (global-set-key "\C-hi" 'counsel-info-lookup-symbol)
-  ;; (global-set-key "\C-hu" 'counsel-unicode-char)
-  (global-set-key "\C-cg" 'counsel-git)
-  (global-set-key "\C-cj" 'counsel-git-grep)
-  (global-set-key "\C-xl" 'counsel-locate)
-  (global-set-key "\C-c\C-r" 'ivy-resume))
+    "/" 'counsel-ag
+    "Th" 'counsel-load-theme)
+  (let ((m ivy-minibuffer-map))
+    (define-key m [escape] 'minibuffer-keyboard-quit)
+    (define-key m (kbd "<s-backspace>") (lambda () (interactive) (kill-line 0))))
+  (require 'ivy-hydra)
+  (define-key ivy-minibuffer-map "\C-o"
+    (defhydra soo-ivy (:hint nil :color pink)
+      "
+ Move     ^^^^^^^^^^ | Call         ^^^^ | Cancel^^ | Options^^ | Action _w_/_s_/_a_: %s(ivy-action-name)
+----------^^^^^^^^^^-+--------------^^^^-+-------^^-+--------^^-+---------------------------------
+ _g_ ^ ^ _k_ ^ ^ _u_ | _f_orward _o_ccur | _i_nsert | _c_alling: %-7s(if ivy-calling \"on\" \"off\") _C_ase-fold: %-10`ivy-case-fold-search
+ ^↨^ _h_ ^+^ _l_ ^↕^ | _RET_ done     ^^ | _q_uit   | _m_atcher: %-7s(ivy--matcher-desc) _t_runcate: %-11`truncate-lines
+ _G_ ^ ^ _j_ ^ ^ _d_ | _TAB_ alt-done ^^ | ^ ^      | _<_/_>_: shrink/grow
+"
+      ;; arrows
+      ("j" ivy-next-line)
+      ("k" ivy-previous-line)
+      ("l" ivy-alt-done)
+      ("h" ivy-backward-delete-char)
+      ("g" ivy-beginning-of-buffer)
+      ("G" ivy-end-of-buffer)
+      ("d" ivy-scroll-up-command)
+      ("u" ivy-scroll-down-command)
+      ("e" ivy-scroll-down-command)
+      ;; actions
+      ("q" keyboard-escape-quit :exit t)
+      ("C-g" keyboard-escape-quit :exit t)
+      ("<escape>" keyboard-escape-quit :exit t)
+      ("C-o" nil)
+      ("i" nil)
+      ("TAB" ivy-alt-done :exit nil)
+      ("C-j" ivy-alt-done :exit nil)
+      ;; ("d" ivy-done :exit t)
+      ("RET" ivy-done :exit t)
+      ("C-m" ivy-done :exit t)
+      ("f" ivy-call)
+      ("c" ivy-toggle-calling)
+      ("m" ivy-toggle-fuzzy)
+      (">" ivy-minibuffer-grow)
+      ("<" ivy-minibuffer-shrink)
+      ("w" ivy-prev-action)
+      ("s" ivy-next-action)
+      ("a" ivy-read-action)
+      ("t" (setq truncate-lines (not truncate-lines)))
+      ("C" ivy-toggle-case-fold)
+      ("o" ivy-occur :exit t))))
 
 (use-package lispy
-  :diminish lispy-mode
   :init (setq lispy-compat '(edebug cider)
 	      ;; Use the same keys as avy for ace jump
 	      lispy-avy-keys sooheon--avy-keys
@@ -216,16 +301,16 @@ current window."
 
 (use-package lispyville
   :diminish lispyville-mode
-  :bind (:map lispyville-mode-map
-              ("M-n" . lispyville-drag-forward)
-              ("M-p" . lispyville-drag-backward))
   :init
   (add-hook 'lispy-mode-hook #'lispyville-mode)
   (setq lispyville-key-theme '(operators
                                (escape insert hybrid emacs)
                                slurp/barf-cp)
         lispyville-motions-put-into-special t
-        lispyville-barf-stay-with-closing t))
+        lispyville-barf-stay-with-closing t)
+  :config
+  (define-key lispyville-mode-map "\M-n" 'lispyville-drag-forward)
+  (define-key lispyville-mode-map "\M-p" 'lispyville-drag-backward))
 
 (use-package magit
   :defer t
@@ -294,6 +379,57 @@ current window."
   (add-to-list 'tramp-default-proxies-alist
                (list (regexp-quote (system-name)) nil nil)))
 
+(use-package undo-tree
+  :diminish undo-tree-mode
+  :init
+  (global-undo-tree-mode)
+  (setq undo-tree-visualizer-timestamps t
+        undo-tree-visualizer-diff t))
+
+(use-package window-numbering
+  :bind (("s-0" . select-window-0)
+         ("s-1" . select-window-1)
+         ("s-2" . select-window-2)
+         ("s-3" . select-window-3)
+         ("s-4" . select-window-4)
+         ("s-5" . select-window-5)
+         ("s-6" . select-window-6))
+  :config
+  (window-numbering-mode 1))
+
+(use-package winner
+  :init
+  (evil-leader/set-key "wu" 'winner-undo)
+  (winner-mode t)
+  (setq winner-boring-buffers
+        (append winner-boring-buffers '("*Compile-Log*"
+                                        "*inferior-lisp*"
+                                        "*Fuzzy Completions*"
+                                        "*Apropos*"
+                                        "*Help*"
+                                        "*cvs*"
+                                        "*Buffer List*"
+                                        "*Ibuffer*"
+                                        "*esh command on file*"))))
+
+(progn ;; personalize
+  (let ((file (expand-file-name (concat (user-real-login-name) ".el")
+                                user-emacs-directory)))
+    (when (file-exists-p file)
+      (load file))))
+
+(defun sooheon--config ()
+  (blink-cursor-mode -1)
+  (global-set-key (kbd "s-u") 'universal-argument)
+  (global-set-key (kbd "s-w") 'delete-window)
+  (global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
+  ;; Fonts
+  (add-to-list 'default-frame-alist
+               '(font . "Input Mono Narrow-12"))
+  (let ((f "fontset-default"))
+    (set-fontset-font f 'hangul '("NanumGothic" . "unicode-bmp"))))
+(sooheon--config)
+
 (progn ;; startup
   (message "Loading %s...done (%.3fs)" user-init-file
            (float-time (time-subtract (current-time)
@@ -305,12 +441,6 @@ current window."
                (float-time (time-subtract (current-time)
                                           before-user-init-time))))
             t))
-
-(progn ;; personalize
-  (let ((file (expand-file-name (concat (user-real-login-name) ".el")
-                                user-emacs-directory)))
-    (when (file-exists-p file)
-      (load file))))
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
