f5fc9704efb1fb867aead1d7e61df60166b6670e
sooheon <tngjs0@gmail.com>
2016-05-30 05:03:20 +0900

    Configure multiple-cursors

--- init.el
+++ init.el
@@ -721,6 +721,34 @@ Will work on both org-mode and any mode that accepts plain html."
     ;; (define-key markdown-mode-map "\M-l" 'markdown-demote)
     )
 
+(use-package multiple-cursors
+  :defer t
+  :init
+  (evil-define-key 'visual global-map "m" 'mc/mark-all-like-this-dwim)
+  ;; Malabarba keybinds
+  ;; http://endlessparentheses.com/multiple-cursors-keybinds.html
+  (define-key ctl-x-map "\C-m" #'mc/mark-all-dwim)
+  (define-key ctl-x-map (kbd "<return>") mule-keymap)
+  (global-set-key (kbd "M-3") #'mc/mark-next-like-this)
+  (global-set-key (kbd "M-4") #'mc/mark-previous-like-this)
+  (global-set-key (kbd "M-#") #'mc/unmark-next-like-this)
+  (global-set-key (kbd "M-$") #'mc/unmark-previous-like-this)
+  (define-prefix-command 'endless/mc-map)
+  ;; C-x m is usually `compose-mail'. Bind it to something
+  ;; else if you use this command.
+  (define-key ctl-x-map "m" 'endless/mc-map)
+  ;; Really really nice!
+  (define-key endless/mc-map "i" #'mc/insert-numbers)
+  (define-key endless/mc-map "h" #'mc-hide-unmatched-lines-mode)
+  (define-key endless/mc-map "a" #'mc/mark-all-like-this)
+  ;; Occasionally useful
+  (define-key endless/mc-map "d" #'mc/mark-all-symbols-like-this-in-defun)
+  (define-key endless/mc-map "r" #'mc/reverse-regions)
+  (define-key endless/mc-map "s" #'mc/sort-regions)
+  (define-key endless/mc-map "l" #'mc/edit-lines)
+  (define-key endless/mc-map "\C-a" #'mc/edit-beginnings-of-lines)
+  (define-key endless/mc-map "\C-e" #'mc/edit-ends-of-lines))
+
 (use-package org
   :defer 10
   :config
