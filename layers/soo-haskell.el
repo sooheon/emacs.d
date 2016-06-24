6eed31748c1c4b34a904353e8476ccf2a5d44f2a
sooheon <tngjs0@gmail.com>
2016-06-25 00:45:08 +0900

    Setup smartparens in haskell

--- layers/soo-haskell.el
+++ layers/soo-haskell.el
@@ -4,7 +4,8 @@
   :init
   (add-hook 'haskell-mode-hook #'intero-mode)
   :config
-  (define-key intero-repl-mode-map [tab] 'dabbrev-expand))
+  (define-key intero-repl-mode-map [tab] 'dabbrev-expand)
+  (add-hook 'intero-repl-mode-hook #'smartparens-mode))
 
 (use-package hindent
   :ensure t
@@ -18,12 +19,15 @@
   :ensure t
   :defer t
   :init
-  (add-hook 'haskell-mode-hook 'structured-haskell-mode))
+  (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+  :config
+  (define-key shm-map "\C-j" 'shm/newline-indent))
 
 ;; Smartparens
 (add-hook 'smartparens-mode-hook
           (lambda ()
             (add-to-list 'sp-no-reindent-after-kill-modes #'haskell-mode)
             (require 'smartparens-haskell)))
+(add-hook 'haskell-mode-hook 'smartparens-mode)
 
 (provide 'soo-haskell)
