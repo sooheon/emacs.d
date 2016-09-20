83fdd0aca86bfbaae9719fc4a6eb227ef29ab1bb
sooheon <tngjs0@gmail.com>
2016-09-20 13:55:16 +0900

    Only bootstrap most important packages
    Install everything else with use-package.

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,105 +1,14 @@
 (defconst emacs-d (file-name-directory (or load-file-name buffer-file-name))
   "The giant turtle on whose shell the world rests.")
 (setq package-user-dir (expand-file-name "elpa" emacs-d))
-(setq package-archives '(("melpa" . "http://melpa.org/packages/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")))
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 (package-refresh-contents)
 
 (defconst soo-packages
-  '(ace-link
-    anaconda-mode
-    auto-compile
-    avy
-    auctex
-    bind-map
-    cargo
-    cdlatex
-    cider
-    circe
-    clojure-mode
-    clj-refactor
-    closql
-    company
-    company-anaconda
-    counsel
-    counsel-projectile
-    dash
-    diff-hl
-    diminish
-    elisp-slime-nav
-    evil
-    evil-commentary
-    evil-cleverparens
-    evil-exchange
-    evil-matchit
-    evil-multiedit
-    evil-numbers
-    evil-snipe
-    evil-surround
-    evil-textobj-anyblock
-    evil-visualstar
-    expand-region
-    finalize
-    flx
-    flycheck
-    flyspell-correct
-    function-args
-    general
-    git-modes
-    gist
-    highlight-escape-sequences
-    hindent
-    hydra
-    jedi
-    lispy
-    lispyville
-    iedit
-    inf-clojure
-    intero
-    ivy
-    ivy-hydra
-    magit
-    markdown-mode
-    ;; multiple-cursors
-    ;; org-plus-contrib
-    osx-dictionary
-    pandoc-mode
-    pdf-tools
-    projectile
-    py-yapf
-    pyenv-mode
-    pyenv-mode-auto
-    queue
-    racer
-    rainbow-delimiters
-    rainbow-mode
-    restclient
-    reveal-in-osx-finder
-    rust-mode
-    sentence-navigation
-    shackle
-    shell-pop
-    shm
-    smartparens
-    smex
-    solarized-theme
-    spacemacs-theme
-    speck
-    super-save
-    swiper
-    term-manager
-    typo
-    undo-tree
+  '(evil
     use-package
-    which-key
-    window-numbering
-    with-editor
-    worf
-    ws-butler
-    zenburn-theme
-    atom-one-dark-theme))
+    general))
 
 ;; install required
 (dolist (package soo-packages)
