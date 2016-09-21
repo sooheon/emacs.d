bc94001927a91bc8c85746dbe0a4798e1d7c71e6
sooheon <tngjs0@gmail.com>
2016-09-21 15:36:15 +0900

    Clean up multiple-cursors bindings

--- init.el
+++ init.el
@@ -913,33 +913,41 @@ INITIAL-INPUT can be given as the initial minibuffer input."
            (er/expand-region arg))))
 
 (use-package multiple-cursors
-  :commands (mc/mark-next-like-this mc/mark-all-dwim)
-  :init
-  (evil-define-key 'visual global-map "m" 'mc/mark-all-like-this-dwim)
-  ;; Malabarba keybinds http://endlessparentheses.com/multiple-cursors-keybinds.html
-  (define-key ctl-x-map "\C-m" #'mc/mark-all-dwim)
-  (define-key ctl-x-map (kbd "<return>") mule-keymap)
-  (global-set-key (kbd "M-3") #'mc/mark-next-like-this)
-  (global-set-key (kbd "M-4") #'mc/mark-previous-like-this)
-  (global-set-key (kbd "M-#") #'mc/unmark-next-like-this)
-  (global-set-key (kbd "M-$") #'mc/unmark-previous-like-this)
+  :preface
   (define-prefix-command 'endless/mc-map)
-  ;; C-x m is usually `compose-mail'. Bind it to something
-  ;; else if you use this command.
-  (define-key ctl-x-map "m" 'endless/mc-map)
-  ;; Really really nice!
-  (define-key endless/mc-map "i" #'mc/insert-numbers)
-  (define-key endless/mc-map "h" #'mc-hide-unmatched-lines-mode)
-  (define-key endless/mc-map "a" #'mc/mark-all-like-this)
-  ;; Occasionally useful
-  (define-key endless/mc-map "d" #'mc/mark-all-symbols-like-this-in-defun)
-  (define-key endless/mc-map "r" #'mc/reverse-regions)
-  (define-key endless/mc-map "s" #'mc/sort-regions)
-  (define-key endless/mc-map "l" #'mc/edit-lines)
-  (define-key endless/mc-map "\C-a" #'mc/edit-beginnings-of-lines)
-  (define-key endless/mc-map "\C-e" #'mc/edit-ends-of-lines))
+  :general
+  (vmap "m" 'mc/mark-all-like-this-dwim)
+  ;; Malabarba keybinds http://endlessparentheses.com/multiple-cursors-keybinds.html
+  (:keymaps 'ctl-x-map
+   "m" 'endless/mc-map                  ; C-x m is usually `compose-mail'.
+   "C-m" #'mc/mark-all-dwim
+   "RET" mule-keymap)
+  ("M-3" #'mc/mark-next-like-this
+   "M-4" #'mc/mark-previous-like-this
+   "M-#" #'mc/unmark-next-like-this
+   "M-$" #'mc/unmark-previous-like-this)
+  (:keymaps 'endless/mc-map
+   ;; Really really nice!
+   "i" #'mc/insert-numbers
+   "h" #'mc-hide-unmatched-lines-mode
+   "a" #'mc/mark-all-like-this
+   ;; Occasionally useful
+   "d" #'mc/mark-all-symbols-like-this-in-defun
+   "r" #'mc/reverse-regions
+   "s" #'mc/sort-regions
+   "l" #'mc/edit-lines
+   "C-a" #'mc/edit-beginnings-of-lines
+   "C-e" #'mc/edit-ends-of-lines))
 
 (use-package hungry-delete :defer t)
+
+(use-package typo
+  :disabled t
+  :init
+  (typo-global-mode 1)
+  (add-hook 'text-mode-hook 'typo-mode)
+  :config
+  (setq-default typo-language 'English))
 
 ;;** Completion and expansion
 (use-package hippie-exp
